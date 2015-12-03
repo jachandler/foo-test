6ce1e4f11799634db3fe29928f2954fbfac75757
Josh Chandler <jachandler.webmail@gmail.com>
2015-12-03 13:52:18 -0500

    Changing test.

--- main.go
+++ main.go
@@ -2,5 +2,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello world.")
+	fmt.Printf("Hello Jenkins!")
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ ! -f "../hello-world.bin" ]; then
+if [ ! -f "hello-world.bin" ]; then
+	echo 'Failed. File not found.'
 	exit -1;
 fi
 
-if [ "$(../hello-world.bin)" -ne "Hello Jenkins!" ]; then
+if [ "$(./hello-world.bin)" != "Hello Jenkins!" ]; then
+	echo 'Failed. Message output was incorrect.';
 	exit -1;
 else
+	echo 'Success.';
 	exit 0;
 fi
